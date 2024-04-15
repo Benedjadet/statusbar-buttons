9d1b20ac941a2c5fcb4b569c98fff53812586285
Benedjadet <107937387+Benedjadet@users.noreply.github.com>
2024-04-15 10:59:50 +0300

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,51 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "statusbar-buttons" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('statusbar-buttons.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from VSCode Statusbar Buttons!');
+	});
+
+	let disposable2 = vscode.commands.registerCommand('statusbar-buttons.bayWorld2', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Bay World from VSCode Statusbar Buttons!');
+	});
+
+
+	let statusBarItem = vscode.window.createStatusBarItem("testButton", vscode.StatusBarAlignment.Left, 1000)
+
+
+	
+
+
+	context.subscriptions.push(statusBarItem)
+
+	statusBarItem.text = "Test button"
+	statusBarItem.command = "statusbar-buttons.bayWorld2"
+	statusBarItem.tooltip = "This is a tip"
+
+	statusBarItem.show()
+
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
+
+
+
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
